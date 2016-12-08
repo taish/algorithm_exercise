29ded318d845c57ff57899db7131984f0037170c
taish <kbystis@gmail.com>
2016-12-08 09:09:15 +0900

    build with xcode

---
+++ algorithm_exercise/algorithm_exercise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm_exercise
+//
+//  Created by KobayashiTaishi on H28/12/08.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
