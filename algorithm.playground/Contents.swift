b6054ad4f35325f3dbeb6a1b8b856398e79ccf0c
taish <kbystis@gmail.com>
2016-12-11 02:40:10 +0900

    solve Knapsack

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,5 +1,93 @@
 //: Playground - noun: a place where people can play
-
 import UIKit
+//DP
+// ナップサック問題
+class Knapsack {
+    struct Pair {
+        // weight
+        var w: Int
+        // value
+        var v: Int
+    }
+    let pairs: [Pair]
+    // 個数
+    var n: Int
+    // 重さの総和
+    var W: Int
 
-var str = "Hello, playground"
+    init() {
+        pairs = [
+            Pair.init(w: 2, v: 3),
+            Pair.init(w: 1, v: 2),
+            Pair.init(w: 3, v: 4),
+            Pair.init(w: 2, v: 2)
+        ]
+        // 個数
+        n  = 4
+        // 重さの総和
+        W = 5
+        //　memo init
+        for num in 0...(n-1) {
+            memo[num] = [:]
+        }
+    }
+
+    func solve() {
+        print("fire")
+        print(self.rec(i: 0, j: W, sum: 0))
+    }
+
+    var memo =  [Int : [Int : Int]]()
+
+
+    // i番目以降の重さの総和がj以下の最大価値
+    func rec(i: Int, j: Int) -> Int {
+        if memo[i]?[j] != nil {
+            return (memo[i]?[j])!
+        }
+        var res: Int
+        if (i == n) {
+            // 残ってない
+            res = 0
+        } else if (j < pairs[i].w) {
+            // この品物は入らない
+            res = rec(i: i + 1, j: j)
+        } else {
+            // 入れない場合と入れる場合を両方試す
+            let left = self.rec(i: i + 1, j: j)
+            let right = self.rec(i: i + 1, j: j - pairs[i].w) + pairs[i].v
+            res = max(left, right)
+        }
+        memo[i]?[j] = res
+        return res
+    }
+
+    func rec(i: Int, j: Int, sum: Int) -> Int {
+        var res: Int
+        if memo[i]?[j] != nil {
+            return (memo[i]?[j])!
+        }
+        if (i == n) {
+            // 残ってない
+            res = sum
+        } else if (j < pairs[i].w) {
+            // この品物は入らない
+            res = rec(i: i + 1, j: j, sum: sum)
+        } else {
+            // 入れない場合と入れる場合を両方試す
+            let left = self.rec(i: i + 1, j: j, sum: sum)
+            let right = self.rec(i: i + 1, j: j - pairs[i].w, sum: sum + pairs[i].v)
+            res = max(left, right)
+        }
+        memo[i]?[j] = res
+        return res
+    }
+
+    func fib(n: Int) -> Int {
+        if (n <= 1) { return n }
+        return fib(n: n-1) + fib(n: n-2)
+    }
+}
+
+Knapsack.init().solve()
+
